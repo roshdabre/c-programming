5ccd8ed6bba6e9272af9307c01f54f6aa3b4ceea
Rosh Dabre <rosh.dabre006@gmail.com>
2025-02-23 19:10:48 +0530

    numbers between 1 to 100

---
+++ ToPrintNoWhileLoop.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    int i;
+    printf("\nEnter the number: \n");
+    i = 1;
+    while (i <= 100)
+    {
+        printf("\t%d", i);
+        i++;
+    }
+getch();
+
+}
